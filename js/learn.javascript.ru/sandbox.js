f9cd04f7efe1b31883738765433851b83db892ce
ytrushkov <yuri.trushkov@onetick.com>
2018-01-30 21:15:19 +0300

    sync with windows

--- js/learn.javascript.ru/sandbox.js
+++ js/learn.javascript.ru/sandbox.js
@@ -1,6 +1,53 @@
 
+'use strict'
+
+
+//var num = 1;
+//var str = "gogoo";
+//var checked = true;
+//var nu = null;
+//var undef ;
+//
+//alert( typeof num );
+//alert( typeof str );
+//alert( typeof checked );
+//alert( typeof nu );
+//alert( typeof undef );
+//alert( typeof {} );
+//alert( typeof function(){} );
+
+//var a = true; // присваивать явно
+//
+//var b = 3 > 4; // или как результат сравнения
+//alert( b ); // false
+//
+//alert( a == b ); 
+
+
+//alert( 'Банан' > 'Аят' );
+//alert( 'Вася' > 'Ваня' ); // true, т.к. 'с' > 'н'
+//alert( 'Привет' > 'Прив' );
+
+//alert( "2" > "14" );
+//alert( +"2" > +"14" ); 
+
+
+//alert( '2' > 1 ); // true, сравнивается как 2 > 1
+//alert( '01' == 1 ); // true, сравнивается как 1 == 1
+//alert( false == 0 ); // true, false становится числом 0
+//alert( true == 1 ); // true, так как true становится числом 1.
+
+
+alert( '' == false ); // true
+alert( 0 === false );
 
 
 
 
-alert("Hi!");
+
+
+
+
+
+
+
